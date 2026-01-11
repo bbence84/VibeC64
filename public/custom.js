a46f8cdd492b5843ff5b0ee7d4a0b4a56eb141ab
Bence Blaske <bence.blaske@sap.com>
2026-01-11 19:15:52 +0100

    Update stats

--- public/custom.js
+++ public/custom.js
@@ -1,11 +1,11 @@
 const script = document.createElement('script');
-script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4S7FCDC77Z'; // URL of the remote file
+script.src = 'https://www.googletagmanager.com/gtag/js?id=G-64XNXQW49P'; // URL of the remote file
 script.onload = () => {
   console.log('Analytics script loaded successfully.');
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-4S7FCDC77Z');  
+  gtag('config', 'G-64XNXQW49P');  
 };
 script.onerror = () => {
   console.error('Error loading analytics script.');
